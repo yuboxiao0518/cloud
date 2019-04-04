efe08661b7c1f44c45bf2f119c118ce55791dc98
yuboxiao0518 <1370035686@qq.com>
2019-04-04 18:04:58 +0800

    commit

---
+++ feign-server/src/main/java/com/example/service/hystrix/SchedualServiceHystrix.java
@@ -0,0 +1,15 @@
+package com.example.service.hystrix;
+
+import org.springframework.stereotype.Component;
+
+import com.example.service.SchedualService;
+@Component
+public class SchedualServiceHystrix implements SchedualService {
+
+	@Override
+	public String homeformClientOne(String name) {
+		// TODO Auto-generated method stub
+		return "Sorry"+name;
+	}
+
+}
