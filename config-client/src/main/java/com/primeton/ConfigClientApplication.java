61c58e0f7ce2566bde0c04cd93db35b50e92a85d
yuboxiao0518 <1370035686@qq.com>
2019-04-11 14:34:34 +0800

    commit

--- config-client/src/main/java/com/primeton/ConfigClientApplication.java
+++ config-client/src/main/java/com/primeton/ConfigClientApplication.java
@@ -2,12 +2,10 @@ package com.primeton;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableEurekaClient
-@RefreshScope
+@EnableDiscoveryClient
 public class ConfigClientApplication {
 
 	public static void main(String[] args) {
