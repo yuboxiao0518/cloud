d2249adf5ac57574229da7465a5c483eadef0250
yuboxiao0518 <1370035686@qq.com>
2019-04-15 10:54:48 +0800

    commit

--- person-server/src/main/java/com/primeton/server/PersionController.java
+++ person-server/src/main/java/com/primeton/server/PersionController.java
@@ -19,7 +19,7 @@ public class PersionController {
     }
 
     @GetMapping("/hi")
-    public String hi(@RequestParam String name) {
+    public String hi(@RequestParam(value="name",defaultValue="yubx") String name) {
         return "hi "+name+",i am from port:" +port;
     }
 }
